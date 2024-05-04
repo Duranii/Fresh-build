148602db5cad1c195424330f7aac4ad9932e5a69
Amishmaqbool <amishmaqbool@gmail.com>
2024-05-05 02:26:38 +0500

    coded

--- src/App.js
+++ src/App.js
@@ -1,23 +1,242 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="parent">
+        <div class="main">
+            <img src="./assets/Logo1.png" alt="" />
+            <p>1-844-769-6377</p>
+        </div>
+    
+        <div class="main1">
+            <img src="./assets/hero_houses.png" alt="" />
+            <div class="discover">
+                <p class="construction">Discover New Construction Homes <br/> Before They Hit The Market</p>
+                <p class="custom">From move-in-ready to custom homes, fresh<strong>builds</strong> is the <br/> modern way to find + buy a new construction house.</p>
+                <button>Search freshbuilds</button>
+            </div>
+        </div>
+        
+        <div class="main2">
+            <div class="eng">
+            <div class="home">
+                <img src="./assets/home.png" alt="" />
+                <p>John S. just closed on a new <br/> home with freshbuilds!</p>
+            </div>
+            <p class="buying">Buying a new home is daunting. <br/><span><strong>fresh</strong>builds</span>  makes it oh-so easy.</p>        
+        
+        
+            <div class="for">
+                <div class="above">
+                    <div class="box1">
+                        <div class="uff">
+                            <img src="./assets/dollar.png" alt="" />
+                            <div class="distance">
+                                <span>Get your time back</span>
+                                <p>Spend time with family, not searching <br/> dozens of listing sites. freshbuilds has <br/> the new construction home in one place.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="box2">
+                        <div class="uff">
+                            <img src="./assets/diamond.png" alt="" />
+                            <div class="distance">
+                                <span>Be a know-it-all</span>
+                                <p>We monitor construction activity and <br/> send you alerts when a new home with <br/> your criteria is ready for its new owners.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <img class="house" src="./assets/house.png" alt="" />
+            </div>
+
+            <div class="rely">
+                <div class="above1">
+                    <div class="box1">
+                        <div class="uff">
+                            <img src="./assets/bulb.png" alt="" />
+                            <div class="distance">
+                                <span>Rely on experts</span>
+                                <p>Our licensed, new construction experts <br/> are by your side every step of the way and <br class="hmm"/> are available 7AM to 7PM – 7 days.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="box2">
+                        <div class="uff">
+                            <img src="./assets/three.png" alt="" />
+                            <div class="distance">
+                                <span>Rest assured</span>
+                                <p>Every freshbuild purchase comes with <br/>a complimentary home warranty giving <br/>you one less thing to worry about.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="find">
+                <div class="new">
+                    <p class="build">Find your new home on <span><strong>fresh</strong>builds</span>.</p>
+                    <p class="largest">freshbuilds has the largest inventory of new construction <br class="jawad"/> homes that you won’t find on other websites. Our proprietary <br class="mahaz"/> technology and network of local real estate agents mean <br class="durani"/> you’ll be the first to hear about new construction projects.</p>
+                    <button>Search freshbuilds</button> <br/>
+                    <img src="./assets/search_houses.png" alt="" />
+                </div>
+                <div>
+                    <div class="circle">
+                        <button class="ago">Listed 2 Hours Ago</button>
+                        <img class="sandy" src="./assets/sandy-millar.png" alt="" />
+                        <div class="west">
+                            <div class="leigh">
+                                <div  class="drive">
+                                    <span>1294 Westleigh Drive</span>
+                                    <p class="bth">3Bd 2Bth 1,524 Sq.ft</p>
+                                    <p class="dollar">$445,000</p>
+                                </div>
+                                <div class="flex">
+                                    <p class="completeion">Completion Date</p>
+                                    <p class="date">09/27/2023</p>
+                                </div>
+                                <div class="flex1">
+                                    <p>Neighborhood</p>
+                                    <img src="./assets/A-.png" alt="" />
+                                </div>
+                                <div class="flex1">
+                                    <p>Schools</p>
+                                    <img src="./assets/B+.png" alt="" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="rec">
+                <div class="box3">
+                    <div class="stars">
+                        <img src="./assets/Stars.png" alt="" />
+                        <p class="bought">We bought our new home while we still living in another state <br/> and needed to find a reputable builder. freshbuilds allowed us to <br/> find new homes that weren’t on other website and once we found <br/> one we liked, they managed the entire process for us.</p>
+                        <span>Steve & Julie S.</span>
+                        <p class="owner">New Homeowners</p>
+                    </div>
+                </div>
+                <div class="box4">
+                    <div class="stars">
+                        <img src="./assets/Stars.png" alt="" />
+                        <p class="bought">I was looking for a new construction home but all of the new <br/> homes on other websites were sold before I could make an offer. <br/> freshbuilds sent me an alert for a new home that was under <br/> construction and helped me write an offer on it!</p>
+                        <span>Tod W.</span>
+                        <p class="owner">New Homeowners</p>
+                    </div>
+                </div>
+           </div>
+
+            <div class="what">
+                <p>Whatever kind of new home you’re looking for, <br/> you’ll find it on <span><strong> fresh</strong>builds</span>.</p>
+            </div>
+
+            <div class="laptop">
+                <div class="luxury">
+                    <div class="grid">
+                        <div class="box5">
+                            <img src="./assets/Condos.png" alt="" />
+                            <p>Condos</p>
+                        </div>
+                        <div class="box5">
+                            <img src="./assets/Townhomes.png" alt="" />
+                            <p>Townhomes</p>
+                        </div>
+                        <div class="box5">
+                            <img src="./assets/Luxury Homes.png" alt="" />
+                            <p>Luxury Homes</p>
+                        </div>
+                        <div class="box5">
+                            <img src="./assets/55+ Community Homes.png" alt="" />
+                            <p>55+ Community Homes</p>
+                        </div>
+                        <div class="box5">
+                            <img src="./assets/Quick Move-In Homes.png" alt="" />
+                            <p>Quick Move-In Homes</p>
+                        </div>
+                        <div class="box5">
+                            <img src="./assets/Ready To Build Land.png" alt="" />
+                            <p>Ready To Build Land</p>
+                        </div>
+                        <div class="box5">
+                            <img src="./assets/Single Family Homes.png" alt="" />
+                            <p>Single Family Homes</p>
+                        </div>
+                        <div class="box5">
+                            <img src="./assets/Rennovated Homes.png" alt="" />
+                            <p>Rennovated Homes</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="battery"><button>Search freshbuilds</button></div>
+            </div>
+            </div>
+        </div>
+
+        <div class="main3">
+                <div class="charger">
+                    <div>
+                        <div class="have">
+                            <p>Have any questions about <span><strong> fresh</strong>builds</span>?</p>
+                        </div>
+                    <div class="hockey">
+                    
+                    <div class="handfree1">
+                        <p>What kind of new construction homes does freshbuilds have on its platform?</p>
+                        <img src="./assets/Vector.png" alt="" />
+                    </div>
+                        <div class="line"></div>
+
+                    <div class="handfree2">
+                        <p>How do I find new construction homes that match my criteria?</p>
+                        <img src="./assets/Vector.png" alt="" />
+                    </div>
+                        <div class="line"></div>
+
+                    <div class="handfree3">
+                        <p>Can I work with freshbuilds if I’m already working with a real estate agent?</p>
+                        <img src="./assets/Vector.png" alt="" />
+                    </div>
+                        <div class="line"></div>
+
+                    <div class="handfree4">
+                        <p>Why should I work with a licensed new construction specialist?</p>
+                        <img src="./assets/Vector.png" alt="" />
+                    </div>
+                        <div class="line"></div>
+
+                    <div class="handfree5">
+                        <p>What factors does freshbuilds consider when evaluating home builders?</p>
+                        <img src="./assets/Vector.png" alt="" />
+                    </div>
+                        <div class="line"></div>
+                        </div>
+                </div>
+                <img class="faq" src="./assets/faq_houses.png" alt="" />
+            </div>
+        </div>
+
+        <div class="main4">
+            <div class="eng">
+            <div class="query">
+                <img src="./assets/Group.png" alt="" />
+                <p>fresh<strong>builds</strong></p>
+            </div>
+            <p class="whites">contact@freshbuilds.com</p>
+            <p class="whites">1-844-769-6377</p>
+            <div class="safety">
+                <div class="super">
+                    <div class="policy">
+                       <p> Privacy Policy</p>
+                       <p> Terms of Service</p>
+                    </div>
+                       <p> Copyright © 2022. All Rights Reserved.</p>
+                </div>
+                <img class="partly" src="./assets/partly.png" alt="" />
+            </div>
+            </div>
+        </div>
     </div>
   );
 }
